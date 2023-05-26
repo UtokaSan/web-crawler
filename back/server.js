83b2552dc2026057a72d0b693e1ef39e98508000
THOLLARD <sebastien.thollard@ynov.com>
2023-05-26 16:35:02 +0200

    ignore

--- back/server.js
+++ back/server.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const module1 = require("./module1")
 
-app.get('/api', async (req, res) => {
-    module1.indexScrapping();
+const { Client } = require('pg');
+const client = new Client({
+user: 'postgres',
+host: 'localhost',
+database: 'postgres',
+password: '123',
+port: 5432,
 });
+client.connect();
 
 app.listen(port, () => {
     console.log('Server app listening on port ' + port);
+});
+
+// Code SQL pour créer la table Produits
+const createTableQuery = `
+    CREATE TABLE Produits(
+        Id SERIAL PRIMARY KEY,
+        Titre VARCHAR(255),
+        Image VARCHAR(255),
+        Prix VARCHAR(50),
+        Texte VARCHAR(500)
+    )
+`;
+
+// Exécute la requête SQL de création de table
+client.query(createTableQuery, (err, res) => {
+    if (err) {
+        console.error('Erreur lors de la création de la table Produits:', err.message);
+    } else {
+        console.log('Table Produits créée avec succès.');
+    }
 });
