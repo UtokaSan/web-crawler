1855dda2f53c89d6201c42b22af7539c0e0945cb
maxime <maxime.poiret@ynov.com>
2023-05-27 17:27:40 +0200

    add: styles + index html

--- back/server.js
+++ back/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const module1 = require("./module1")
+const scrapeLetterboxd = require("./scrapingLetterboxd")
 
 app.get('/api', async (req, res) => {
-    module1.indexScrapping();
+    const text = await scrapeLetterboxd.scrapeLetterboxd();
+    res.json(text);
 });
 
 app.listen(port, () => {
--- js/js.js
+++ js/js.js
@@ -2,8 +2,6 @@ $(function () {
 
     'use strict';
 
-
-
     $('.menu__item').on("mouseenter", function () {
         let id = $(this).data('id');
         $('#' + id + '-bg__img').addClass('active');
@@ -13,3 +11,21 @@ $(function () {
     });
 
 });
+
+
+    // Sélectionnez les boutons "Sites De Vente" et "Réseaux Sociaux"
+    var sitesDeVenteBtn = document.getElementById('sites-de-vente');
+    var reseauxSociauxBtn = document.getElementById('reseaux-sociaux');
+
+    // Ajoutez un gestionnaire d'événement au bouton "Sites De Vente"
+    sitesDeVenteBtn.addEventListener('click', function (event) {
+    event.preventDefault(); // Empêche le comportement par défaut du lien
+    window.location.href = 'sites-de-vente.html'; // Redirige vers la page "sites-de-vente.html"
+});
+
+    // Ajoutez un gestionnaire d'événement au bouton "Réseaux Sociaux"
+    reseauxSociauxBtn.addEventListener('click', function (event) {
+    event.preventDefault(); // Empêche le comportement par défaut du lien
+    window.location.href = 'reseaux-sociaux.html'; // Redirige vers la page "reseaux-sociaux.html"
+});
+
