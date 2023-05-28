9325a90a6f2f3638b8f3f0fe7b8fa483dd102eeb
Florian Boulbes <florian.boulbes@ynov.com>
2023-05-28 22:41:58 +0200

    update: connectweb organize searching

--- back/public/js/connectWeb.js
+++ back/public/js/connectWeb.js
@@ -1,9 +1,27 @@
 fetch("http://localhost:3000/api/produitsamazon")
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        const div = document.querySelector("#test");
-        div.innerHTML = data[0].titre;
+        console.log(data)
+        const testselec = document.querySelector("#test")
+        const divElement = document.createElement('div');
+        divElement.classList.add('item-amazon');
+        for (let i = 0; i < 5; i++) {
+            const produit = data[i];
+            const itemClass = `item-${i + 1}`;
+            const divElement = document.createElement('div');
+            divElement.classList.add(itemClass);
+            divElement.classList.add('item-amazon');
+            const paragraph = document.createElement('p');
+            paragraph.textContent = produit.titre;
+            const priceParagraph = document.createElement('p');
+            priceParagraph.textContent = produit.prix;
+            const imgElement = document.createElement('img');
+            imgElement.src = produit.image;
+            divElement.appendChild(paragraph);
+            divElement.appendChild(priceParagraph);
+            divElement.appendChild(imgElement);
+            testselec.appendChild(divElement);
+        }
     })
     .catch(error => {
         console.error(error);
