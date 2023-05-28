b7bd9554f6081ea89f65a07cbb6e16bf2714a4cd
Florian Boulbes <florian.boulbes@ynov.com>
2023-05-28 23:49:54 +0200

    update: organize item amazon/aliexpress

--- back/public/js/connectWeb.js
+++ back/public/js/connectWeb.js
@@ -2,7 +2,7 @@ fetch("http://localhost:3000/api/produitsamazon")
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        const testselec = document.querySelector("#test")
+        const amazonSelect = document.querySelector("#main-amazon")
         const divElement = document.createElement('div');
         divElement.classList.add('item-amazon');
         for (let i = 0; i < 5; i++) {
@@ -20,12 +20,43 @@ fetch("http://localhost:3000/api/produitsamazon")
             divElement.appendChild(paragraph);
             divElement.appendChild(priceParagraph);
             divElement.appendChild(imgElement);
-            testselec.appendChild(divElement);
+            amazonSelect.appendChild(divElement);
         }
     })
     .catch(error => {
         console.error(error);
     });
+
+fetch("http://localhost:3000/api/produitsaliexpress")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        const aliexpressSelec = document.querySelector("#main-aliexpress")
+        const divElement = document.createElement('div');
+        divElement.classList.add('item-aliexpress');
+        for (let i = 0; i < 5; i++) {
+            const produit = data[i];
+            const itemClass = `item-${i + 1}`;
+            const divElement = document.createElement('div');
+            divElement.classList.add(itemClass);
+            divElement.classList.add('item-aliexpress');
+            const paragraph = document.createElement('p');
+            paragraph.textContent = produit.titre;
+            const priceParagraph = document.createElement('p');
+            priceParagraph.textContent = produit.prix;
+            const imgElement = document.createElement('img');
+            imgElement.src = produit.image;
+            divElement.appendChild(paragraph);
+            divElement.appendChild(priceParagraph);
+            divElement.appendChild(imgElement);
+            aliexpressSelec.appendChild(divElement);
+        }
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+
 
 fetch ("http://localhost:3000/crawler/sale")
     .then(response => response.json())
